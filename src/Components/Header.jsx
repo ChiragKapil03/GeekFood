9796d1fa3cc754fb9e95c55b284bd8219e2ec208
Chirag Kapil <your.email@example.com>
2024-09-27 15:43:10 +0530

    full proj

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,24 @@
+import React from "react";
+import styles from "./Header.module.css"
+
 const Header = ()=>{
     return(
         <>
-            <h1>This is Header</h1>
+            <div className={styles.header}>
+                <div className={styles.left}>
+                    <h2>GeekFood</h2>
+                </div>
+                <ul className={styles.center}>
+                    <li className={styles.res}>Home</li>
+                    <li>Quote</li>
+                    <li>Resturants</li>
+                    <li>Foods</li>
+                    <li>Contact</li>
+                </ul>
+                <div className={styles.right}>
+                    <button id="btn1">Get Started</button>
+                </div>
+            </div>
         </>
     )
 }
